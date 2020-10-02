96f318a06e0f7b8922b851304d2d7b7efbfdb2d7
Shannon Dias <saidshannon@gmail.com>
2020-10-02 14:15:46 +0530

    Final commit

---
+++ Course 4 - Using databases with Python/ex15/ex15_multi-table_database_tracks.py
@@ -0,0 +1,113 @@
+#===============================================================================
+# This application will read an iTunes export file in XML and produce a properly
+# normalized database.
+#===============================================================================
+
+# Importing libraries
+import xml.etree.ElementTree as ET
+import sqlite3
+
+#--- Creating and connecting to database
+conn = sqlite3.connect('trackdb.sqlite')
+cur = conn.cursor()
+
+#--- Initialising database
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+''')
+cur.executescript('''
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY 
+        AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    genre_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+);
+''')
+
+#--- Asking user to input the data file name and provide 'Library.xml' as a fallback
+fname = input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'Library.xml'
+
+#--- The structure of the of the interesting objects in the XML
+# <key>Track ID</key><integer>369</integer>
+# <key>Name</key><string>Another One Bites The Dust</string>
+# <key>Artist</key><string>Queen</string>
+
+#--- Defining a function to lookup the value of a 'key' tag
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found : return child.text
+        if child.tag == 'key' and child.text == key :
+            found = True
+    return None
+
+#--- Opening and parsing the file
+stuff = ET.parse(fname)
+#--- Selecting the 'dict' tags in the third depth and counting them
+all = stuff.findall('dict/dict/dict')
+print('Dict count:', len(all))
+#--- Looping through all result elements 'dict'
+for entry in all:
+    #--- Trying to look up the different data fields. If not found, moving on to the next element
+    if ( lookup(entry, 'Track ID') is None ) : continue
+
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    album = lookup(entry, 'Album')
+    genre = lookup(entry, 'Genre')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+
+    #--- If a data field is not found, then move on to the next element
+    if name is None or artist is None or album is None or genre is None: 
+        continue
+
+    #--- Printing the data field of the search result element for the user
+    print(name, artist, album, genre, count, rating, length)
+
+    #--- Updating the relevant tables with the data field of the search result element
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (artist_id, title) 
+        VALUES ( ?, ? )''', ( artist_id, album ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, genre_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''', 
+        ( name, album_id, genre_id, length, rating, count ) )
+
+    #--- Committing the changes
+    conn.commit()
---
+++ Course 4 - Using databases with Python/ex16/where.js
@@ -0,0 +1,20 @@
+myData = [
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[33.4641541,-111.9231478, '1475 N Scottsdale Rd, Scottsdale, AZ 85257, USA'],
+[38.0399391,23.8030901, 'Monumental Plaza, Building C, 1st Floor, Leof. Kifisias 44, Marousi 151 25, Greece'],
+[28.3639976,75.58696809999999, 'VidyaVihar Campus, Pilani, Rajasthan 333031, India'],
+[6.8919631,3.7186605, 'Ilishan Remo Ogun State Nigeria, ILISHAN REMO, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Banaras Hindu University Campus, Varanasi, Uttar Pradesh 221005, India'],
+[12.9503878,77.5022224, 'Mysore Road, Jnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.549841,-97.1143146, '1301 S University Parks Dr, Waco, TX 76706, USA'],
+[39.9619537,116.3662615, '19 Xinjiekou Outer St, BeiTaiPingZhuang, Haidian Qu, Beijing Shi, China, 100875'],
+[53.8930389,27.5455567, 'praspiekt Niezaliežnasci 4, Minsk, Belarus'],
+[44.8184339,20.4575676, 'Studentski trg 1, Beograd, Serbia'],
+[42.5030333,-89.0309048, '700 College St, Beloit, WI 53511, USA'],
+[53.8930389,27.5455567, 'praspiekt Niezaliežnasci 4, Minsk, Belarus'],
+[10.6779085,78.74454879999999, 'Palkalaiperur, Tiruchirappalli, Tamil Nadu 620024, India'],
+[42.3504997,-71.1053991, 'Boston, MA 02215, USA'],
+[47.486135,19.057964, 'Budapest, Fővám tér 8., 1093 Hungary'],
+[35.3050053,-120.6624942, 'San Luis Obispo, CA 93407, USA']
+];
